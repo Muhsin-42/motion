548c40fdaac3115344af04ccd2c3f3320393117e
Muhsin-42 <muhsinofficial21@gmail.com>
2024-05-11 16:44:55 +0530

    Login and signup complete

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,125 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormSchema } from "@/lib/types";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../../../../public/logo.svg";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Loader from "@/components/global/loader";
+import { login as actionLoginUser } from "@/lib/serverActions/auth-actions";
+import { toast } from "sonner";
+const Login = () => {
+  const router = useRouter();
+  const [submitError, setSubmitError] = useState("");
+  const form = useForm<z.infer<typeof FormSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(FormSchema),
+    defaultValues: { email: "", password: "" },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (
+    formData
+  ) => {
+    const { error, errorMsg } = await actionLoginUser(formData);
+    if (error) {
+      toast.error(errorMsg);
+      setSubmitError(errorMsg);
+    } else {
+      toast.success("Buckle up, you're back in motion! 🚀");
+      router.replace("/");
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        onChange={() => {
+          if (submitError) setSubmitError("");
+        }}
+        className="flex w-full flex-col space-y-6 sm:w-[400px] sm:justify-center"
+      >
+        <Link
+          href="/"
+          className="   
+          flex
+          w-full
+          items-center
+          justify-start"
+        >
+          <Image src={Logo} alt="motion Logo" width={50} height={50} />
+          <span
+            className="text-4xl
+          font-semibold first-letter:ml-2 dark:text-white"
+          >
+            motion.
+          </span>
+        </Link>
+        <FormDescription
+          className="
+        text-foreground/60"
+        >
+          An all-In-One Collaboration and Productivity Platform
+        </FormDescription>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="email" placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="password" placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {submitError && <FormMessage>{submitError}</FormMessage>}
+        <Button
+          type="submit"
+          className="w-full p-6"
+          size="lg"
+          disabled={isLoading}
+        >
+          {!isLoading ? "Login" : <Loader />}
+        </Button>
+        <span className="self-center">
+          Dont have an account?{" "}
+          <Link href="/signup" className="text-primary">
+            Sign Up
+          </Link>
+        </span>
+      </form>
+    </Form>
+  );
+};
+
+export default Login;
